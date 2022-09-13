dd991e394e101b20c20f0f1f00f41dd5ff8e5bac
MaramNaqeeb <maramnaqib1998@gmail.com>
2022-09-13 14:55:19 +0300

    user2

--- _python/python_fundamentals/User/user.py
+++ _python/python_fundamentals/User/user.py
@@ -3,40 +3,28 @@ class user:
         self.name=name
         self.email=email_address
         self.account_balance=0
+      
 
     def make_deposit (self,amount):
         self.account_balance += amount
-
+        return self
     def make_withdrawal(self, amount):
         self.account_balance -= amount
-
+        return self
     def display_user_balance(self):
         print(self.name, self.account_balance)
-
+        return self
     def transfer(self, other_user, amount):
         other_user.account_balance += amount 
         self.account_balance -= amount
+        return self
 
-saeed= user("saeed","saeed12@gmail.com")
-saeed.make_deposit(1000)
-saeed.make_deposit(1000)
-saeed.make_deposit(1000)
-saeed.make_withdrawal(500)
-saeed.display_user_balance()
+saeed= user("saeed","saeed12@gmail.com").make_deposit(1000).make_deposit(1000).make_deposit(1000).make_withdrawal(500).display_user_balance()
 
 Hasan= user("Hasan","Hasan44@gmail.com")
-Hasan.make_deposit(2000)
-Hasan.make_deposit(2000)
-Hasan.make_withdrawal(800)
-Hasan.make_withdrawal(800)
-Hasan.display_user_balance()
+Hasan.make_deposit(2000).make_deposit(2000).make_withdrawal(800).make_withdrawal(800).display_user_balance()
 
-Maram= user("Maram","maramnaqib1998@gmail.com")
-Maram.make_deposit(4000)
-Maram.make_withdrawal(2000)
-Maram.make_withdrawal(2000)
-Maram.make_withdrawal(2000)
-Maram.display_user_balance()
+Maram= user("Maram","maramnaqib1998@gmail.com").make_deposit(4000).make_withdrawal(2000).make_withdrawal(2000).make_withdrawal(2000).display_user_balance()
 
 saeed.transfer(Maram,500)
 saeed.display_user_balance()
