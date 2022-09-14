83d3b037b6adeda64ba1653ebc342931347643f6
MaramNaqeeb <maramnaqib1998@gmail.com>
2022-09-14 16:24:38 +0300

    loop

--- _python/python_fundamentals/For Loops/loops.py
+++ _python/python_fundamentals/For Loops/loops.py
@@ -1,31 +1,45 @@
 # //first
-for i in range(0,150+1):
+for i in range(0,151):
     print(i)
 # //second
-for i in range(5,1001):
- if (i%5==0):
+for i in range(5,1001,5):
     print(i)
 # //third
-for i in range(1,1000):
-   if (i % 10 ==0):
+for i in range(1,101):
+   if (i % 10 == 0):
        print("Coding Dojo")
-   elif (i % 5 ==0):
+   elif (i % 5 == 0):
        print("Coding")
    else:
       print(i)
+
 # //forth
-sum=0
-for i in range(0,500000):
- if (i%2!=0): 
-    sum=(sum+i)
-    print(sum+i)
+my_result=0
+for i in range(0,500001):
+    my_result+=i;
+    my_result=my_result+i
+    # i++
+    # i=i+1
+    # i+=1
+print(my_result)
+
+
 # //fifth
 for i in range(2018,0,-4):
    print(i)
+
+
 # //sixth
 lowNum=1
-highNum=10
-mult=2
-for i in range(2,10,2):
+highNum=20
+mult=3
+for i in range(3,20,3):
  print (i) 
 
+# method 2
+low = 1
+high = 40
+multi = 4
+for i in range (low,high):
+    if i%3 == 0:
+        print (i)
